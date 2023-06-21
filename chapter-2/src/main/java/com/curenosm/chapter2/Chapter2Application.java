95e5b583a5aed214142426884ef9d0e4f1149dcd
Misael Cureño <curenosm@gmail.com>
2023-06-21 09:08:42 -0600

    Capitulo 2 terminado

--- chapter-2/src/main/java/com/curenosm/chapter2/Chapter2Application.java
+++ chapter-2/src/main/java/com/curenosm/chapter2/Chapter2Application.java
@@ -3,7 +3,15 @@ package com.curenosm.chapter2;
 import java.util.Arrays;
 import java.util.Comparator;
 import java.util.List;
+import java.util.Optional;
+import java.util.function.BiConsumer;
 import java.util.function.Consumer;
+import java.util.function.DoubleSupplier;
+import java.util.function.Function;
+import java.util.function.Predicate;
+import java.util.stream.Collectors;
+import java.util.stream.Stream;
+import lombok.Builder;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.boot.ApplicationRunner;
 import org.springframework.boot.SpringApplication;
@@ -24,16 +32,147 @@ public class Chapter2Application {
     return args -> {
       // For each es un consumer
       //
+      List<String> strings = Arrays.asList(
+        "this", "is", "a", "list");
+
+      strings.forEach(new Consumer<String>() {
+        @Override
+        public void accept (String o) {
+          System.out.println(o);
+        }
+      });
+
+      strings.forEach(s -> System.out.println(s)); // Lambda expression is the signature
+      strings.forEach(System.out::println);
+
+      Stream<String> stream = Stream.of("1", "2", "3");
+      Stream<String> stream2 = Stream.of("1", "2", "3");
+      Stream<String> stream3 = Stream.of("1", "6", "3");
+
+      Optional<String> op = Optional.of("sop");
+      op.ifPresent(System.out::println);
+      stream.forEach(System.out::println);
+
+      // peek() Returns a stream with the same elements that stream2
+      stream2
+        .peek(System.out::println)
+        .forEach(System.out::println);
+
+      stream3.forEachOrdered(System.out::println);
+
     };
   }
 
-}
 
-class CustomConsumer implements Consumer {
+  @Bean
+  public ApplicationRunner suppliers () {
+    return args -> {
+      DoubleSupplier randomSupplier = new DoubleSupplier() {
+        @Override
+        public double getAsDouble () {
+          return Math.random();
+        }
+      };
 
-  @Override
-  public void accept (Object o) {
+      randomSupplier = () -> Math.random();
+      randomSupplier = Math::random;
+      log.info("{}", randomSupplier);
 
+      Optional<Double> opt = Optional.of(0.5);
+      Double val = opt.orElseGet(Math::random);
+
+      List<String> names = Arrays.asList(
+        "Zoe", "Simon", "Peter", "Sarah", "Jayne"
+      );
+
+      Optional<String> first = names.stream()
+        .filter(name -> name.startsWith("C"))
+        .findFirst();
+
+      System.out.println(first);
+      System.out.println(first.orElse(
+        String.format(
+          "No result found in %s",
+          names.stream().collect(Collectors.joining(", "))
+        )
+      ));
+
+      System.out.println(first.orElseGet(() ->
+        String.format(
+          "No result found in %s",
+          String.join(", ", names)
+          )
+      ));
+    };
   }
+
+
+  @Bean
+  public ApplicationRunner predicates() {
+    return args -> {
+      String[] names = Stream.of("buen", "nombre")
+        .toList()
+        .toArray(new String[]{});
+
+      System.out.println(Arrays.toString(names));
+
+    };
+  }
+
+
+  @Bean
+  public ApplicationRunner functions() {
+    return args -> {
+      String[] names = Stream.of("buen", "nombre")
+        .toList()
+        .toArray(new String[]{});
+
+      List<Integer> nameLengths = Arrays.stream(names)
+        .map(new Function<String, Integer>() {
+          @Override
+          public Integer apply (String s) {
+            return s.length();
+          }
+        })
+        .toList();
+
+      nameLengths = Arrays
+          .stream(names)
+          .map(s -> s.length())
+          .toList();
+
+      /*
+      Primitive variations
+
+        - IntFunction
+        - DoubleFunction
+        - LongFunction
+        - ToIntFunction
+        - ToDoubleFunction
+        - ToLongFunction
+        - DoubleToIntFunction
+        ...
+
+        - UnaryOperator
+        - BinaryOperator
+        - IntBinaryOperator
+        ...
+
+        - BiFunction
+        - ToIntBiFunction
+        - ToLongBiFunction
+        ...
+       */
+
+      nameLengths = Arrays
+        .stream(names)
+        .map(String::length)
+        .toList();
+
+      System.out.printf("nameLengths = %s%n", nameLengths);
+
+    };
+  }
+
 
 }
--- chapter-2/src/test/java/com/curenosm/chapter2/Chapter2ApplicationTests.java
+++ chapter-2/src/test/java/com/curenosm/chapter2/Chapter2ApplicationTests.java
@@ -1,13 +1,64 @@
 package com.curenosm.chapter2;
 
+import com.curenosm.chapter2.ImplementPredicate;
+import java.util.stream.Stream;
+import org.junit.jupiter.api.BeforeAll;
+import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
+
+import static com.curenosm.chapter2.ImplementPredicate.*;
+import static org.junit.jupiter.api.Assertions.assertEquals;
 import org.springframework.boot.test.context.SpringBootTest;
 
-@SpringBootTest
 class Chapter2ApplicationTests {
 
+  private ImplementPredicate demo = new ImplementPredicate();
+  private String[] names;
+
+  @BeforeEach
+  public void setUp() {
+    names = Stream
+      .of("Maria", "Wash", "Kaylee", "Sarah", "Simon")
+      .sorted()
+      .toArray(String[]::new);
+  }
+
   @Test
-  void contextLoads () {
+  void getNamesOfLength5 () throws Exception {
+    assertEquals(
+      "Maria, Sarah, Simon",
+      demo.getNamesOfLength(5, names)
+    );
+  }
+
+  @Test
+  void getNamesStartingWith5 () throws Exception {
+    assertEquals(
+      "Sarah, Simon",
+      demo.getNamesStartingWith("S", names)
+    );
+  }
+
+  @Test
+  void getNamesSatisfyingCondition() throws Exception {
+    assertEquals("Maria, Sarah, Simon",
+      demo.getNamesSatisfyingCondition(s -> s.length() == 5, names));
+    assertEquals("Sarah, Simon",
+      demo.getNamesSatisfyingCondition(s -> s.startsWith("S"), names));
+    assertEquals("Maria, Sarah, Simon",
+      demo.getNamesSatisfyingCondition(LENGTH_FIVE, names));
+    assertEquals("Sarah, Simon",
+      demo.getNamesSatisfyingCondition(STARTS_WITH_S, names));
+  }
+
+  @Test
+  void composedPredicate() throws Exception {
+    assertEquals("Sarah, Simon",
+      demo.getNamesSatisfyingCondition(LENGTH_FIVE.and(STARTS_WITH_S), names));
+    assertEquals("Maria, Sarah, Simon",
+      demo.getNamesSatisfyingCondition(LENGTH_FIVE.or(STARTS_WITH_S), names));
+    assertEquals("Kaylee, Wash",
+      demo.getNamesSatisfyingCondition(LENGTH_FIVE.negate(), names));
   }
 
 }
